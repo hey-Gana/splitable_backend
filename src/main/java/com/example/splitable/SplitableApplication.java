27a4188593e0cf386e1263a3c1ca093154fe614e
Ganapathi Subramaniam Sankaranarayanan <119105395+hey-Gana@users.noreply.github.com>
2025-10-23 09:16:04 -0400

    Initial Commit

---
+++ src/main/java/com/example/splitable/SplitableApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitableApplication.class, args);
+	}
+
+}
