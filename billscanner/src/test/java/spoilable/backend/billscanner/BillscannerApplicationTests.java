7b4726d9049c3d3839a78ec6ed258775ee790a86
hey-Gana <ganasubs98@gmail.com>
2025-12-26 16:33:28 -0500

    Initial Commit - Spring Boot Setup

---
+++ billscanner/src/test/java/spoilable/backend/billscanner/BillscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package spoilable.backend.billscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillscannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
